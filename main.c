da241e5bc080077605ae01b1c164d472f1a6dd33
Amandine Demuynck <amdemuyn@c3r11s5.42madrid.com>
2023-03-31 19:18:11 +0200

    tests with images

--- main.c
+++ main.c
@@ -1,22 +1,20 @@
 #include <mlx.h>
 
-typedef struct	s_vars {
-	void	*mlx;
-	void	*win;
-}				t_vars;
-
-int	close(int keycode, t_vars *vars)
-{
-	mlx_destroy_window(vars->mlx, vars->win);
-	return (0);
-}
-
 int	main(void)
 {
-	t_vars	vars;
+	void	*mlx;
+	void	*mlx_win;
+	void	*img;
+	char	*relative_path = "./tumb.xpm";
+	int		img_width;
+	int		img_height;
 
-	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 1920, 1080, "Hello world!");
-	mlx_hook(vars.win, 33, 1L<<22, close, &vars);
-	mlx_loop(vars.mlx);
+	img_width = 500;
+	img_height = 500;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 500, 500, "images");
+	img = mlx_xpm_file_to_image(mlx, relative_path, &img_width, &img_height);
+	mlx_put_image_to_window(mlx, mlx_win, img, 0, 0);
+	mlx_loop(mlx);
 }
